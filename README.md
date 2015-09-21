64ea43497785932563e8844d9618485f160b06c4
whitequark <whitequark@whitequark.org>
2015-09-21 04:45:51 +0300

    Add documentation.

--- README.md
+++ README.md
@@ -4,8 +4,6 @@ rust-xdg
 rust-xdg is a library that makes it easy to follow the X Desktop Group
 standards for data, configuration, cache and runtime directories.
 
-See also [documentation](https://whitequark.github.io/rust-xdg/xdg/).
-
 Installation
 ------------
 
@@ -19,39 +17,7 @@ xdg = "*"
 Examples
 --------
 
-To configure paths for application `myapp`:
-
-```rust
-extern crate xdg;
-let xdg_dirs = xdg::BaseDirectories::with_prefix("myapp");
-```
-
-To store configuration:
-
-```rust
-let config_path = xdg_dirs.place_config_file("config.ini")
-                          .expect("cannot create configuration directory");
-let mut config_file = try!(File::create(config_path));
-try!(write!(&mut config_file, "configured = 1"));
-```
-
-The `config.ini` file will appear in the proper location for desktop
-configuration files, most likely `~/.config/myapp/config.ini`.
-The leading directories will be automatically created.
-
-To retrieve supplementary data:
-
-```rust
-let logo_path = xdg_dirs.find_config_file("logo.png")
-                        .expect("application data not present");
-let mut logo_file = try!(File::open(logo_path));
-let mut logo = Vec::new();
-try!(logo_file.read_to_end(&mut logo));
-```
-
-The `logo.png` will be searched in the proper locations for
-supplementary data files, most likely `~/.local/share/myapp/logo.png`,
-then `/usr/local/share/myapp/logo.png` and `/usr/share/myapp/logo.png`.
+See [documentation](https://whitequark.github.io/rust-xdg/xdg/struct.BaseDirectories.html).
 
 License
 -------
@@ -59,4 +25,5 @@ License
 rust-xdg is distributed under the terms of both the MIT license
 and the Apache License (Version 2.0).
 
-See LICENSE-APACHE and LICENSE-MIT for details.
+See [LICENSE-APACHE][LICENSE-APACHE] and [LICENSE-MIT][LICENSE-MIT]
+for details.
--- src/xdg.rs
+++ src/xdg.rs
@@ -9,6 +9,59 @@ use std::io::Result as IoResult;
 use std::fs::PathExt;
 use std::os::unix::fs::PermissionsExt;
 
+/// BaseDirectories allows to look up paths to configuration, data,
+/// cache and runtime files in well-known locations according to
+/// the [X Desktop Group Base Directory specification][xdg-basedir].
+/// [xdg-basedir]: http://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html
+///
+/// The Base Directory specification defines four kinds of files:
+///
+///   * **Configuration files** store the application's settings and
+///     are often modified during runtime;
+///   * **Data files** store supplementary data, such as graphic assets,
+///     precomputed tables, documentation, or architecture-independent
+///     source code;
+///   * **Cache files** store non-essential, transient data that provides
+///     a runtime speedup;
+///   * **Runtime files** include filesystem objects such are sockets or
+///     named pipes that are used for communication internal to the application.
+///     Runtime files must not be accessible to anyone except current user.
+///
+/// # Examples
+///
+/// To configure paths for application `myapp`:
+///
+/// ```
+/// extern crate xdg;
+/// let xdg_dirs = xdg::BaseDirectories::with_prefix("myapp");
+/// ```
+///
+/// To store configuration:
+///
+/// ```
+/// let config_path = xdg_dirs.place_config_file("config.ini")
+///                           .expect("cannot create configuration directory");
+/// let mut config_file = try!(File::create(config_path));
+/// try!(write!(&mut config_file, "configured = 1"));
+/// ```
+///
+/// The `config.ini` file will appear in the proper location for desktop
+/// configuration files, most likely `~/.config/myapp/config.ini`.
+/// The leading directories will be automatically created.
+///
+/// To retrieve supplementary data:
+///
+/// ```
+/// let logo_path = xdg_dirs.find_data_file("logo.png")
+///                         .expect("application data not present");
+/// let mut logo_file = try!(File::open(logo_path));
+/// let mut logo = Vec::new();
+/// try!(logo_file.read_to_end(&mut logo));
+/// ```
+///
+/// The `logo.png` will be searched in the proper locations for
+/// supplementary data files, most likely `~/.local/share/myapp/logo.png`,
+/// then `/usr/local/share/myapp/logo.png` and `/usr/share/myapp/logo.png`.
 pub struct BaseDirectories {
     prefix: PathBuf,
     data_home: PathBuf,
@@ -21,10 +74,26 @@ pub struct BaseDirectories {
 
 impl BaseDirectories
 {
+    /// Reads the process environment, determines the XDG base directories,
+    /// and returns a value that can be used for lookup.
+    /// The following environment variables are examined:
+    ///
+    ///   * `HOME`; if not set: panic.
+    ///   * `XDG_DATA_HOME`; if not set: assumed to be `$HOME/.local/share`.
+    ///   * `XDG_CONFIG_HOME`; if not set: assumed to be `$HOME/.config`.
+    ///   * `XDG_CACHE_HOME`; if not set: assumed to be `$HOME/.cache`.
+    ///   * `XDG_DATA_DIRS`; if not set: assumed to be `/usr/local/share:/usr/share`.
+    ///   * `XDG_CONFIG_DIRS`; if not set: assumed to be `/etc/xdg`.
+    ///   * `XDG_RUNTIME_DIR`; if not accessible or permissions are not `0700`: panic.
+    ///
+    /// As per specification, if an environment variable contains a relative path,
+    /// the behavior is the same as if it was not set.
     pub fn new() -> BaseDirectories {
         BaseDirectories::with_env("", &|name| env::var(name))
     }
 
+    /// Same as [`new()`](#method.new), but `prefix` is implicitly prepended to
+    /// every path that is looked up.
     pub fn with_prefix<P>(prefix: P) -> BaseDirectories where P: AsRef<Path> {
         BaseDirectories::with_env(prefix, &|name| env::var(name))
     }
@@ -99,97 +168,161 @@ impl BaseDirectories
         }
     }
 
+    /// Returns `true` if `XDG_RUNTIME_DIR` is available, `false` otherwise.
     pub fn has_runtime_directory(&self) -> bool {
         self.runtime_dir.is_some()
     }
-    fn get_runtime_directory(&self) -> &Path {
+
+    fn get_runtime_directory(&self) -> &PathBuf {
         self.runtime_dir.as_ref().expect("$XDG_RUNTIME_DIR must be set")
     }
 
-    pub fn place_data_file<P>(&self, path: P) -> IoResult<PathBuf>
-            where P: AsRef<Path> {
-        write_file(&self.data_home, self.prefix.join(path))
-    }
+    /// Given a relative path `path`, returns an absolute path in
+    /// `XDG_CONFIG_HOME` where a configuration file may be stored.
+    /// Leading directories in the returned path are pre-created;
+    /// if that is not possible, an error is returned.
     pub fn place_config_file<P>(&self, path: P) -> IoResult<PathBuf>
             where P: AsRef<Path> {
         write_file(&self.config_home, self.prefix.join(path))
     }
+
+    /// Like [`place_config_file()`](#method.place_config_file), but for
+    /// a data file in `XDG_DATA_HOME`.
+    pub fn place_data_file<P>(&self, path: P) -> IoResult<PathBuf>
+            where P: AsRef<Path> {
+        write_file(&self.data_home, self.prefix.join(path))
+    }
+
+    /// Like [`place_config_file()`](#method.place_config_file), but for
+    /// a cache file in `XDG_CACHE_HOME`.
     pub fn place_cache_file<P>(&self, path: P) -> IoResult<PathBuf>
             where P: AsRef<Path> {
         write_file(&self.cache_home, self.prefix.join(path))
     }
+
+    /// Like [`place_config_file()`](#method.place_config_file), but for
+    /// a runtime file in `XDG_RUNTIME_DIR`.
+    /// If `XDG_RUNTIME_DIR` is not available, panics.
     pub fn place_runtime_file<P>(&self, path: P) -> IoResult<PathBuf>
             where P: AsRef<Path> {
         write_file(self.get_runtime_directory(), self.prefix.join(path))
     }
 
-    pub fn find_data_file<P>(&self, path: P) -> Option<PathBuf>
-            where P: AsRef<Path> {
-        read_file(&self.data_home, &self.data_dirs,
-                  self.prefix.join(path))
-    }
+    /// Given a relative path `path`, returns an absolute path to an existing
+    /// configuration file, or `None`. Searches `XDG_CONFIG_HOME` and then
+    /// `XDG_CONFIG_DIRS`.
     pub fn find_config_file<P>(&self, path: P) -> Option<PathBuf>
             where P: AsRef<Path> {
         read_file(&self.config_home, &self.config_dirs,
                   self.prefix.join(path))
     }
+
+    /// Given a relative path `path`, returns an absolute path to an existing
+    /// configuration file, or `None`. Searches `XDG_DATA_HOME` and then
+    /// `XDG_DATA_DIRS`.
+    pub fn find_data_file<P>(&self, path: P) -> Option<PathBuf>
+            where P: AsRef<Path> {
+        read_file(&self.data_home, &self.data_dirs,
+                  self.prefix.join(path))
+    }
+
+    /// Given a relative path `path`, returns an absolute path to an existing
+    /// configuration file, or `None`. Searches `XDG_CACHE_HOME`.
     pub fn find_cache_file<P>(&self, path: P) -> Option<PathBuf>
             where P: AsRef<Path> {
         read_file(&self.cache_home, &Vec::new(),
                   self.prefix.join(path))
     }
+
+    /// Given a relative path `path`, returns an absolute path to an existing
+    /// runtime file, or `None`. Searches `XDG_RUNTIME_DIR`.
+    /// If `XDG_RUNTIME_DIR` is not available, panics.
     pub fn find_runtime_file<P>(&self, path: P) -> Option<PathBuf>
             where P: AsRef<Path> {
         read_file(self.get_runtime_directory(), &Vec::new(),
                   self.prefix.join(path))
     }
 
-    pub fn create_data_directory<P>(&self, path: P) -> IoResult<PathBuf>
-            where P: AsRef<Path> {
-        create_directory(&self.data_home,
-                         self.prefix.join(path))
-    }
+    /// Given a relative path `path`, returns an absolute path to a configuration
+    /// directory in `XDG_CONFIG_HOME`. The directory and all directories
+    /// leading to it are created if they did not exist;
+    /// if that is not possible, an error is returned.
     pub fn create_config_directory<P>(&self, path: P) -> IoResult<PathBuf>
             where P: AsRef<Path> {
         create_directory(&self.config_home,
                          self.prefix.join(path))
     }
+
+    /// Like [`create_config_directory()`](#method.create_config_directory),
+    /// but for a data directory in `XDG_DATA_HOME`.
+    pub fn create_data_directory<P>(&self, path: P) -> IoResult<PathBuf>
+            where P: AsRef<Path> {
+        create_directory(&self.data_home,
+                         self.prefix.join(path))
+    }
+
+    /// Like [`create_config_directory()`](#method.create_config_directory),
+    /// but for a cache directory in `XDG_CACHE_HOME`.
     pub fn create_cache_directory<P>(&self, path: P) -> IoResult<PathBuf>
             where P: AsRef<Path> {
         create_directory(&self.cache_home,
                          self.prefix.join(path))
     }
+
+    /// Like [`create_config_directory()`](#method.create_config_directory),
+    /// but for a runtime directory in `XDG_RUNTIME_DIR`.
+    /// If `XDG_RUNTIME_DIR` is not available, panics.
     pub fn create_runtime_directory<P>(&self, path: P) -> IoResult<PathBuf>
             where P: AsRef<Path> {
         create_directory(self.get_runtime_directory(),
                          self.prefix.join(path))
     }
 
-    pub fn list_data_files<P>(&self, path: P) -> Vec<PathBuf>
-            where P: AsRef<Path> {
-        list_files(&self.data_home, &self.data_dirs,
-                   self.prefix.join(path))
-    }
-    pub fn list_data_files_once<P>(&self, path: P) -> Vec<PathBuf>
-            where P: AsRef<Path> {
-        list_files_once(&self.data_home, &self.data_dirs,
-                        self.prefix.join(path))
-    }
+    /// Given a relative path `path`, list absolute paths to all files
+    /// in directories with path `path` in `XDG_CONFIG_HOME` and
+    /// `XDG_CONFIG_DIRS`.
     pub fn list_config_files<P>(&self, path: P) -> Vec<PathBuf>
             where P: AsRef<Path> {
         list_files(&self.config_home, &self.config_dirs,
                    self.prefix.join(path))
     }
+
+    /// Like [`list_config_files`](#method.list_config_files), but
+    /// only the first occurence of every distinct filename is returned.
     pub fn list_config_files_once<P>(&self, path: P) -> Vec<PathBuf>
             where P: AsRef<Path> {
         list_files_once(&self.config_home, &self.config_dirs,
                         self.prefix.join(path))
     }
+
+    /// Given a relative path `path`, lists absolute paths to all files
+    /// in directories with path `path` in `XDG_DATA_HOME` and
+    /// `XDG_DATA_DIRS`.
+    pub fn list_data_files<P>(&self, path: P) -> Vec<PathBuf>
+            where P: AsRef<Path> {
+        list_files(&self.data_home, &self.data_dirs,
+                   self.prefix.join(path))
+    }
+
+    /// Like [`list_data_files`](#method.list_data_files), but
+    /// only the first occurence of every distinct filename is returned.
+    pub fn list_data_files_once<P>(&self, path: P) -> Vec<PathBuf>
+            where P: AsRef<Path> {
+        list_files_once(&self.data_home, &self.data_dirs,
+                        self.prefix.join(path))
+    }
+
+    /// Given a relative path `path`, lists absolute paths to all files
+    /// in directories with path `path` in `XDG_CACHE_HOME`.
     pub fn list_cache_files<P>(&self, path: P) -> Vec<PathBuf>
             where P: AsRef<Path> {
         list_files(&self.cache_home, &Vec::new(),
                    self.prefix.join(path))
     }
+
+    /// Given a relative path `path`, lists absolute paths to all files
+    /// in directories with path `path` in `XDG_RUNTIME_DIR`.
+    /// If `XDG_RUNTIME_DIR` is not available, panics.
     pub fn list_runtime_files<P>(&self, path: P) -> Vec<PathBuf>
             where P: AsRef<Path> {
         list_files(self.get_runtime_directory(), &Vec::new(),
@@ -197,7 +330,7 @@ impl BaseDirectories
     }
 }
 
-fn write_file<P>(home: &Path, path: P) -> IoResult<PathBuf>
+fn write_file<P>(home: &PathBuf, path: P) -> IoResult<PathBuf>
         where P: AsRef<Path> {
     match path.as_ref().parent() {
         Some(parent) => try!(fs::create_dir_all(home.join(parent))),
@@ -206,14 +339,14 @@ fn write_file<P>(home: &Path, path: P) -> IoResult<PathBuf>
     Ok(PathBuf::from(home.join(path.as_ref())))
 }
 
-fn create_directory<P>(home: &Path, path: P) -> IoResult<PathBuf>
+fn create_directory<P>(home: &PathBuf, path: P) -> IoResult<PathBuf>
         where P: AsRef<Path> {
     let full_path = home.join(path.as_ref());
     try!(fs::create_dir_all(&full_path));
     Ok(full_path)
 }
 
-fn read_file<P>(home: &Path, dirs: &Vec<PathBuf>, path: P) -> Option<PathBuf>
+fn read_file<P>(home: &PathBuf, dirs: &Vec<PathBuf>, path: P) -> Option<PathBuf>
         where P: AsRef<Path> {
     let full_path = home.join(path.as_ref());
     if full_path.exists() {
@@ -228,7 +361,7 @@ fn read_file<P>(home: &Path, dirs: &Vec<PathBuf>, path: P) -> Option<PathBuf>
     None
 }
 
-fn list_files<P>(home: &Path, dirs: &Vec<PathBuf>, path: P) -> Vec<PathBuf>
+fn list_files<P>(home: &PathBuf, dirs: &Vec<PathBuf>, path: P) -> Vec<PathBuf>
         where P: AsRef<Path> {
     iter::once(home)
         .chain(dirs.iter())
@@ -242,7 +375,7 @@ fn list_files<P>(home: &Path, dirs: &Vec<PathBuf>, path: P) -> Vec<PathBuf>
         .fold(vec![], |mut accum, paths| { accum.extend(paths); accum })
 }
 
-fn list_files_once<P>(home: &Path, dirs: &Vec<PathBuf>, path: P) -> Vec<PathBuf>
+fn list_files_once<P>(home: &PathBuf, dirs: &Vec<PathBuf>, path: P) -> Vec<PathBuf>
         where P: AsRef<Path> {
     let mut seen = std::collections::HashSet::new();
     list_files(home, dirs, path).into_iter().filter(|path| {
